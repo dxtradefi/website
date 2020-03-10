7930d0b6998f259552fde9da67f348dde408a830
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2020-03-10 16:09:39 +0200

    Доработка функциональности карусели.

--- boat/js/script.js
+++ boat/js/script.js
@@ -48,34 +48,54 @@ function DOMloaded () {
   // КАРУСЕЛЬ КАРТИНОК
   // СМ. прототип с комментариями
 
-  const carusel = document.querySelector('#image-carusel')
+  let carusel = document.querySelector('#carusel')
   let caruselWidth = carusel.getBoundingClientRect().width
-  const collage = document.querySelector('#image-collage')
+  const collage = document.querySelector('#collage')
   const collageWidth = collage.getBoundingClientRect().width
-  let posInitial
+  const nextBtn = document.getElementById('next-btn')
+  const prevBtn = document.getElementById('prev-btn')
+  const slideWidth = collage.children[0].getBoundingClientRect().width
+  const slideLenght = collage.children.length
+  const marginRight = 20
+  var posInitial
+  let posFinal
   let posX1 = 0
   let posX2 = 0
   let doubleTouch = false
   
-    // Событие нажатия мыши
+  // Событие нажатия мыши
   collage.addEventListener('mousedown', startAction)
   
-    // Событие нажатия пальцем
+  // Событие нажатия пальцем
   collage.addEventListener('touchstart', startAction)
   collage.addEventListener('touchmove', moveAction)
   collage.addEventListener('touchend', endAction)
-
-    // Событие двойного нажатия (открыть модальное окно)
+  
+  // Событие двойного нажатия (открыть модальное окно)
   collage.addEventListener('dblclick', openModalWindow)
   collage.addEventListener('touchstart', openModalWindow)
-    
+  
+  // Событие кнопок контроля
+  nextBtn.addEventListener('click', btnAction.bind(null, 'next'))
+  prevBtn.addEventListener('click', btnAction.bind(null, 'prev'))
+  
+  collage.addEventListener('transitioned', animaSlide)
 
+    // Добавляем анимацию при переходах или клаках
+  function animaSlide() {
+    collage.classList.add('slide-anim')
+  } 
+    // Функция событие при нажатии на карусель
   function startAction (e) {
       // Ignored attempt to cancel a touchstart event with cancelable=false
     if (e.cancelable) {
       e.preventDefault();
     }
+      // Удаляем анимацию при перетаскивании
+      // !!! ВНИМАНИЕ если не удалить, то перетаскиваться не будет
+    collage.classList.remove('slide-anim')
 
+      // Ширина карусели при смене ориентации
     if (screen.orientation.type === 'portrait-primary') {
       caruselWidth = carusel.getBoundingClientRect().width
     } else {
@@ -100,7 +120,7 @@ function DOMloaded () {
   }
   
   
-
+    // Фунция собития при перетаскивании карусели
   function moveAction (e) {
     if (e.type === 'touchmove') {
         // Предыдущая позиция - текущая позиция
@@ -112,40 +132,129 @@ function DOMloaded () {
       posX1 = e.clientX
     }
 
-      // Отнимаем от текущей позиции элемента, полученое смещение курсора
+      // Текущая позиция колажа при передвижении
+      posInitial = collage.offsetLeft
+      // Конечная позиция колажа 
+      // Ширина колажа, ширини карусели, правый отступ 20px
+    posFinal = -(collageWidth - caruselWidth - marginRight)
+      // Центр одной ячейки слайда
+    const halfSlide = (slideWidth + marginRight)
+
+      // Ограничить, зона за пределы которой не выходит колаж
+    if (posInitial > 0 ) {
+      collage.style.left = 0 + 'px'
+      return
+    }
+    if (posInitial < posFinal) {
+      collage.style.left = posFinal + 'px'
+      return
+    }
+    
+      // Класс для контроллера 
+    if (posInitial > -halfSlide ) {
+      prevBtn.classList.add('hide')
+    } else  {
+      prevBtn.classList.remove('hide')
+    } 
+
+    if (posInitial < (posFinal + halfSlide)) {
+      nextBtn.classList.add('hide')
+    } 
+    else  {
+      nextBtn.classList.remove('hide')
+    }
+
+      // Отнимаем от текущей позиции элемента
+      // полученое смещение курсора
     collage.style.left = `${collage.offsetLeft - posX2}px`
   }
   
-  
-  function endAction (e) {
+    // Функия при отжатии мыши/пальца от карусели
+  function endAction () {
+        // Добавляем transition
+      animaSlide()
+      // Начальная позиция
+      posInitial = collage.offsetLeft
       // Конецовка последнего слайда
       // по отношению к правому краю карусели
       // Ширина коллажа - ширина карусели - 20px margin-right
-
-    if (e.cancelable) {
-      e.preventDefault();
+    posFinal = -(collageWidth - caruselWidth - marginRight)
+      // Слайд с правым отступом
+    const slide = (slideWidth + marginRight)
+    
+      // Если смещение меньше половины начального слайда
+      // Возвращаем к началу
+    if (posInitial > -(slide / 2)) {
+      btnAction('prev')
+      collage.style.left = `${0}px`
+      prevBtn.classList.add('hide')
+    } 
+      // Если смещение больше половины начального слайда
+      // Возвращаем к следующему слайду
+    else if (posInitial < -(slide / 2) && posInitial >= -slide) {
+      prevBtn.classList.remove('hide')
+      collage.style.left = `-${slide}px`
     }
 
-    let endOfSlides = -(collageWidth - caruselWidth)
-    // console.log(endOfSlides);
-    
-
-      // Если смещение больше 0, возвращаем к началу
-    if (collage.offsetLeft >= 0) {
-      collage.style.left = `${15}px`
-    }
-    
-    // Если смещение больше endOfSlides
-    // возвращаем к ndOfSlides
-    if (collage.offsetLeft <= endOfSlides) {
-      collage.style.left = `${endOfSlides}px`
+      // Если смещение больше половины последнего слайда
+      // возвращаем к posFinal
+    if (posInitial < (posFinal + (slide / 2))) {
+      btnAction('next')      
+      collage.style.left = `${posFinal}px`
+      nextBtn.classList.add('hide')
+    } 
+      // Если смещение меньше половины последнего слайда
+      // возвращаем к предыдущему слайду
+    else if (posInitial < (posFinal + slide)){
+      nextBtn.classList.remove('hide')
+      collage.style.left = `${posFinal + slide}px`
     }
       // Сбрасываем событие нажатия
     document.onmouseup = null
     document.onmousemove = null
   }
 
-  
+    // Стрелочный контроллер (кнопки - стрелки)
+  function btnAction(dir) {
+    posInitial = collage.offsetLeft
+
+    let steps = caruselWidth / (slideWidth + marginRight)
+    let eachStep = slideLenght - steps
+      // Слайд с правым отступом
+    const slide = (slideWidth + marginRight)
+      // Текущая позиция колажа при передвижении
+    let limiter = collage.offsetLeft - slideWidth
+      
+      // Конечная позиция колажа
+    posFinal = -(collageWidth - caruselWidth - marginRight)
+
+    animaSlide()
+    
+      if (dir === 'next') {
+      
+      collage.style.left = `${posInitial - slide}px`
+      prevBtn.classList.remove('hide')
+
+      if (posInitial <= (posFinal + slide)) {
+        nextBtn.classList.add('hide')
+        collage.style.left = `${posFinal}px`
+      } 
+    }
+    
+    if (dir === 'prev') {
+      
+      collage.style.left = `${posInitial + slide}px`
+      nextBtn.classList.remove('hide')
+
+      if (posInitial >= -(slide)) {
+        prevBtn.classList.add('hide')
+        collage.style.left = `${0}px`
+      }
+    }
+  }
+
+
+    // МОДАЛЬНОЕ ОКНО
   function openModalWindow (e) {
       
       // Проверяем на касание
@@ -175,7 +284,6 @@ function DOMloaded () {
     }
   }
   function modalWindow (e) {
-    console.log(e);
     
     const body = e.target.closest('body')
     const main = e.target.closest('main')
