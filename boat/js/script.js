beb829f45f65f3dd437beef53f6f812819d172f8
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2020-03-16 17:07:47 +0200

    Закоментил строку, подправил css, оптимизировал чутка js

--- boat/js/script.js
+++ boat/js/script.js
@@ -1,4 +1,3 @@
-// window.onload = function () {
 window.addEventListener('DOMContentLoaded', DOMloaded)
 
 function DOMloaded () {
@@ -39,11 +38,6 @@ function DOMloaded () {
       }
     })
   }
-
-
-
-
-
 
   // КАРУСЕЛЬ КАРТИНОК
   // СМ. прототип с комментариями
@@ -261,63 +255,19 @@ function DOMloaded () {
             },300)
           }
           if (doubleTouch > 1) {
-            // console.log('double touch');
-            modalWindow(e)
+              // Импортирование модального окна
+              // Не грузим личней код без надобности
+            import('./modal.js').then(modal => {
+              modal.modalWindow(e)
+            })
             doubleTouch = false
           }
         },300)
       }
     } else {
-      modalWindow(e)
-      // console.log('double click');
-    }
-  }
-  function modalWindow (e) {
-    
-    const body = e.target.closest('body')
-    const main = e.target.closest('main')
-      // Проверка на наличие ссылки на картинку
-    if (e.target.getAttribute('src') === null) {
-      return
-    }
-    const link = e.target.getAttribute('src')
-    
-    // Создаем элементы
-    const modal = document.createElement('div')
-    const block = document.createElement('figure')
-    const image = document.createElement('img')
-    const close = document.createElement('span')
-
-
-    modal.setAttribute('class', 'modal')
-    block.setAttribute('id', 'modal-box')
-    image.setAttribute('src', `${link}`)
-    image.setAttribute('draggable', false)
-    close.setAttribute('class', 'close-btn')
-
-    block.appendChild(image)
-    // block.appendChild(close)
-    modal.appendChild(block)
-    modal.appendChild(close)
-    body.insertBefore(modal, main)
-    setTimeout(() => {
-      modal.classList.add('show-modal')
-    }, 100)
-    body.style.overflow = 'hidden'
-
-    modal.addEventListener('click', closeModal)
-    // close.addEventListener('click', closeModal)
-    // console.log(e.target.closest('body'));
-    function closeModal (e) {
-      body.removeAttribute('style')
-      
-      if (e.target.className === '') {
-        return
-      }
-      modal.classList.remove('show-modal')
-      setTimeout(() => {
-        modal.remove()
-      }, 300)
+      import('./modal.js').then(modal => {
+        modal.modalWindow(e)
+      })
     }
   }
 }
